67f6215cde3cb2d3e0cab2cda14128fc88745e6f
mintkat <85989407+m1ntkat@users.noreply.github.com>
2022-05-08 01:57:54 -0800

    Added Lua Module

---
+++ export_templates.bat
@@ -0,0 +1,3 @@
+scons -j8 platform=windows tools=no target=release bits=32
+scons -j8 platform=windows tools=no target=release_debug bits=64
+scons -j8 platform=windows tools=no target=release bits=64
